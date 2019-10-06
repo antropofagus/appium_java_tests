23391d51edf9f5c94a7c0977bc55ae8eeec1923f
antropofagus <fc9201714562@gmail.com>
2019-10-06 16:21:47 +0300

    Ex3: cancel search test

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
     private AppiumDriver driver;
@@ -21,7 +22,7 @@ public class FirstTest {
 
         capabilities.setCapability("platformVersion", "9");
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "emulator-5556");
+        capabilities.setCapability("deviceName", "emulator-5554");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("app", "/Users/elcentipede/IdeaProjects/appium_java_test/apks/org.wikipedia.apk");
         capabilities.setCapability("appActivity", ".main.MainActivity");
@@ -59,6 +60,38 @@ public class FirstTest {
 
     }
 
+    @Test
+    public void searchResultsShouldNotBeDisplayedAfterSearchCancel() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Can`t find 'Search Wikipedia' input",
+                5
+        );
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Can`t find search input",
+                "Java",
+                5
+        );
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text, 'Java')]"),
+                "Can`t find search result",
+                5
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Can`t find 'x' button",
+                5
+        );
+        Assert.assertTrue("search result is still visivle",
+                waitForElementNotPresent(
+                    By.id("org.wikipedia:id/fragment_search_results"),
+                    "Search results are still visible",
+                    5
+            )
+        );
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeout) {
         WebDriverWait wait = new WebDriverWait(driver, timeout);
         wait.withMessage(error_message + "\n");
@@ -66,10 +99,23 @@ public class FirstTest {
                 ExpectedConditions.presenceOfElementLocated(by)
         );
     }
-
+    private boolean waitForElementNotPresent(By by, String error_message, long timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
     private WebElement waitForElementAndClick(By by, String error_message, long timeout) {
         WebElement element = waitForElementPresent(by, error_message, timeout);
         element.click();
         return element;
     }
+    private WebElement waitForElementAndSendKeys(By by, String error_message, String value, long timeout) {
+         WebElement element = waitForElementPresent(by, error_message, timeout);
+         element.sendKeys(value);
+         return element;
+    }
+
+
 }
